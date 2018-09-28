7074d6f08a325440b561efe1d622a18b1ccebe7d
zhangyijun03 <zhangyijun03@baidu.com>
2018-09-28 21:32:37 +0800

    tkhd box解析

--- app/mp4/box/tkhd.js
+++ app/mp4/box/tkhd.js
@@ -20,17 +20,18 @@ module.exports = async function (fd, box) {
         box.modificationTime = buffer.readUInt64BE(20);
         box.trackId = buffer.readUInt32BE(28);
         box.duration = buffer.readUInt64BE(36);
-        position = 48;
+        position = 52;
     } else {
         box.creationTime = buffer.readUInt32BE(12);
         box.modificationTime = buffer.readUInt32BE(16);
         box.trackId = buffer.readUInt32BE(20);
         box.duration = buffer.readUInt32BE(28);
-        position = 36;
+        position = 40;
     }
     box.layer = buffer.readUInt16BE(position);
     box.alternateGroup = buffer.readUInt16BE(position + 2);
-    box.volume = buffer.readUInt16BE(position + 2);
-
+    box.volume = buffer.readUInt16BE(position + 4);
+    box.width = buffer.readUInt32BE(position + 44) / 65536;
+    box.height = buffer.readUInt32BE(position + 48) / 65536;
 
 }
--- app/static/box/renderer.js
+++ app/static/box/renderer.js
@@ -1,5 +1,6 @@
 const raw = require('../tpl/raw.etpl');
 const mvhd = require('../tpl/mvhd.etpl');
+const tkhd = require('../tpl/tkhd.etpl');
 
 exports.raw = function (box) {
     return raw({raw: JSON.stringify(box, null, 2)});
@@ -14,5 +15,16 @@ exports.mvhd = function (box) {
         dts: box.duration,
         r: Math.round(box.rate / 65536 * 100) + '%',
         v: Math.round(box.volume / 256 * 100) + '%'
+    });
+};
+
+exports.tkhd = function (box) {
+    return tkhd({
+        ct: new Date(box.creationTime * 1000).toLocaleDateString(),
+        mt: new Date(box.modificationTime * 1000).toLocaleDateString(),
+        dts: box.duration,
+        v: Math.round(box.volume / 256 * 100) + '%',
+        w: box.width,
+        h: box.height
     });
 };
